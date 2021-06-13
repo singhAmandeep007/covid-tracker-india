a8fba2354b2acc9c7a2fd9e895e7967d1ad85266
Grimm0803 <55043082+amandeepmicro@users.noreply.github.com>
2021-06-13 18:34:28 +0530

    line chart remade

--- src/components/Charts/LineChart.js
+++ src/components/Charts/LineChart.js
@@ -1,352 +1,135 @@
-import React, { Component } from "react";
+import React,{ useRef, useLayoutEffect } from "react";
+import { renderToStaticMarkup } from "react-dom/server";
 import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+import {stringToDate} from './../../utils/util';
 
-class LineChart extends Component {
+/* Enable theme(s) */
+am4core.useTheme(am4themes_animated);
 
-  chart = null;
+function LineChart({casesInfo}) {
 
-  componentDidUpdate() {
-    //console.log('linear chart updated')
-    this.chart.dispose();
-    this.setConfig();
-  }
-  componentDidMount() {
-    this.setConfig()
-  }
-  shouldComponentUpdate(nextProps, nextState) {
-    return this.props.weeklyData !== nextProps.weeklyData
-  }
-  componentWillUnmount() {
-    if (this.chart) {
-      this.chart.dispose();
-    }
-  }
+  const casesChart = useRef(null);
 
-
-  renderChartUsingConfig = (weeklyData) => {
-    //console.log('weeklyData: ', weeklyData)
-
-    const data = weeklyData.map((d) => {
-      return {
-        confirmed: d.dailyconfirmed,
-        recovered: d.dailyrecovered,
-        death: d.dailydeceased,
-        confirmVal: d.dailyconfirmed,
-        recoverVal: d.dailyrecovered,
-        deathVal: d.dailydeceased,
-        date: d.dateymd,
-        id: 1
-      }
-    })
-    console.log('data: ', data);
-
-    const getMaxY = function (data) {
-      let arr = [];
-      for (let i of data) {
-        let max = Math.max(i.confirmed, i.recovered, i.death)
-        arr.push(max)
-      }
-      return Math.max(...arr)
-    }
-
-    return {
-      data: data,
-      mouseWheelBehavior: "none",
-      xAxes: [
-        {
-          height: 15,
-          type: "CategoryAxis",
-          id: "dateAxis",
-          startLocation: 0.5,
-          endLocation: 0.5,
-          dataFields: {
-            category: "date"
-          },
-          title: {
-            text: "Date",
-            fontSize: "12px",
-            fill: "#666666",
-            marginTop: "10px"
-          },
-          renderer: {
-            inside: false,
-            labels: {
-              disabled: true,
-              template: {
-                disabled: true
-              }
-            },
-            grid: {
-              disabled: true
-            }
-          },
-          tooltipDateFormat: "MMM dd, yyyy",
-          tooltip: {
-            background: {
-              fill: "#000",
-              strokeWidth: 0,
-              cornerRadius: 4,
-              fillOpacity: 0.7
-            },
-            filters: [
-              {
-                type: "DropShadowFilter",
-                dx: 1,
-                dy: 1,
-                blur: 3,
-                color: "#373737"
-              }
-            ]
-          }
-        }
-      ],
-      yAxes: [
-        {
-          type: "ValueAxis",
-          max: getMaxY(data) + 50000,
-          min: 0,
-          strictMinMax: true,
-          id: "areaAxis",
-          cursorTooltipEnabled: false,
-          tooltip: {
-            disabled: true
-          },
-          title: {
-            text: "Cases",
-            fontSize: "12px",
-            fill: "#666666",
-            marginTop: "10px"
-          },
-          renderer: {
-            opposite: true,
-            labels: {
-              disabled: true
-            },
-            grid: {
-              stroke: "#dee0e1",
-              strokeWidth: 1,
-              strokeOpacity: 0.5
-            },
-            baseGrid: {
-              disabled: true
-            }
-          },
-
-        }
-      ],
-      series: [
-        {
-          id: "confirmed",
-          name: "Confirmed",
-          type: "LineSeries",
-          dataFields: {
-            valueY: "confirmVal",
-            categoryX: "date"
-          },
-          stroke: "#fa0504",
-          strokeWidth: 2,
-          yAxis: "areaAxis",
-          tooltipText: "Confirmed: {confirmed}",
-          tooltip: {
-            getFillFromObject: false,
-            background: {
-              fill: "#000",
-              strokeWidth: 0,
-              fillOpacity: 0.7,
-              cornerRadius: 4
-            },
-            fontSize: "12px",
-            filters: [
-              {
-                type: "DropShadowFilter",
-                dx: 1,
-                dy: 1,
-                blur: 3,
-                color: "#373737"
-              }
-            ]
-          },
-          fillOpacity: 0.1,
-          fill: am4core.color("#fa0504"),
-          segments: {
-            fillModifier: {
-              type: "LinearGradientModifier",
-              opacities: [1, 0.1],
-              offsets: [0, 0.5, 1],
-              gradient: {
-                rotation: 90
-              }
-            }
-          }
-        },
-        {
-          id: "recovered",
-          name: "Recovered",
-          type: "LineSeries",
-          dataFields: {
-            valueY: "recoverVal",
-            categoryX: "date"
-          },
-          zIndex: 0,
-          stroke: "#009900",
-          strokeWidth: 2,
-          yAxis: "areaAxis",
-          tooltipText: "Recovered: {recovered}",
-          tooltip: {
-            getFillFromObject: false,
-            background: {
-              fill: "#000",
-              strokeWidth: 0,
-              fillOpacity: 0.7,
-              cornerRadius: 4
-            },
-            fontSize: "12px",
-            filters: [
-              {
-                type: "DropShadowFilter",
-                dx: 1,
-                dy: 1,
-                blur: 3,
-                color: "#373737"
-              }
-            ]
-          },
-          fillOpacity: 0.1,
-          fill: am4core.color("#009900"),
-          segments: {
-            fillModifier: {
-              type: "LinearGradientModifier",
-              opacities: [1, 0.1],
-              offsets: [0, 0.5, 1],
-              gradient: {
-                rotation: 90
-              }
-            }
-          }
-        },
-        {
-          id: "deaths",
-          name: "Deaths",
-          type: "LineSeries",
-          dataFields: {
-            valueY: "deathVal",
-            categoryX: "date"
-          },
-          zIndex: 0,
-          stroke: "#666666",
-          strokeWidth: 2,
-          yAxis: "areaAxis",
-          tooltipText: "Deaths: {death}",
-          tooltip: {
-            getFillFromObject: false,
-            background: {
-              fill: "#000",
-              strokeWidth: 0,
-              fillOpacity: 0.7,
-              cornerRadius: 4
-            },
-            fontSize: "12px",
-            filters: [
-              {
-                type: "DropShadowFilter",
-                dx: 1,
-                dy: 1,
-                blur: 3,
-                color: "#373737"
-              }
-            ]
-          },
-          fillOpacity: 0.1,
-          fill: am4core.color("#666666"),
-          segments: {
-            fillModifier: {
-              type: "LinearGradientModifier",
-              opacities: [1, 0.1],
-              offsets: [0, 0.5, 1],
-              gradient: {
-                rotation: 90
-              }
-            }
-          }
-        },
-
-      ],
-      cursor: {
-        xAxis: "dateAxis",
-        lineX: {
-          stroke: "#9eaec9",
-          strokeWidth: 2,
-          strokeDasharray: ""
-        },
-        lineY: {
-          disabled: "true"
-        },
-        behavior: "none"
-      },
-      legend: {
-        position: "top",
-        paddingTop: 0,
-        marginTop: -10,
-        contentAlign: "left",
-        paddingBottom: 10,
-        fontSize: 12,
-        useDefaultMarker: true,
-        itemContainers: {
-          clickable: false,
-          focusable: false,
-          cursorOverStyle: [
-            {
-              property: "cursor",
-              value: "default"
-            }
-          ]
-        },
-        markers: {
-          width: 18,
-          children: [
-            {
-              width: 16,
-              height: 16,
-              valign: "middle",
-              horizontalAlign: "center",
-              cornerRadiusTopLeft: 12,
-              cornerRadiusTopRight: 12,
-              cornerRadiusBottomRight: 12,
-              cornerRadiusBottomLeft: 12,
-              strokeWidth: 2,
-              strokeOpacity: 1,
-              stroke: "#fff"
-            }
-          ]
-        }
-      }
-    };
-  };
-
-
-  setConfig() {
-    const config = this.renderChartUsingConfig(this.props.weeklyData)
-    this.chart = am4core.createFromConfig(
-      config,
-      "chart2",
-      am4charts.XYChart
-    );
-  }
-
-
-
-  render() {
+  function Tooltip({ name, value }) {
     return (
-
-      <div
-        id="demo-chart"
-        className="chart2"
-        style={{ height: "290px" }}
-      />
-
+      <div>
+        {name}: {value}
+      </div>
     );
   }
+
+  useLayoutEffect(() => {
+    let x = am4core.create("casesChartDiv", am4charts.XYChart);
+    // data
+    const casesData = casesInfo.slice(-365).map((d) => {
+      return {
+        date:stringToDate(d.dateymd,"yyyy-MM-dd",'-'),
+        dailyconfirmed:d.dailyconfirmed,
+        dailydeceased:d.dailydeceased,
+        dailyrecovered:d.dailyrecovered,
+        totalconfirmed:d.totalconfirmed,
+        totaldeceased:d.totaldeceased,
+        totalrecovered:d.totalrecovered
+      }
+    });
+    x.data = casesData;
+
+    let dateAxis = x.xAxes.push(new am4charts.DateAxis());
+    dateAxis.renderer.grid.template.location = 0;
+    dateAxis.tooltipDateFormat = "MMM dd, yyyy";
+
+    let valueAxis = x.yAxes.push(new am4charts.ValueAxis());
+    valueAxis.tooltip.disabled = true;
+    valueAxis.renderer.minWidth = 35;
+
+    // Gradient
+    let fillModifier = new am4core.LinearGradientModifier();
+    fillModifier.opacities = [1, 0.1];
+    fillModifier.offsets = [0,0.5, 1];
+    fillModifier.gradient.rotation = 90;
+    // confirmed
+    let seriesConfirmed = x.series.push(new am4charts.LineSeries());
+    seriesConfirmed.name = "Confirmed";
+    seriesConfirmed.dataFields.dateX = "date";
+    seriesConfirmed.dataFields.valueY = "dailyconfirmed";
+    seriesConfirmed.stroke = "#db2828";
+    seriesConfirmed.fill = "#db2828";
+
+    seriesConfirmed.tooltipHTML = renderToStaticMarkup(
+      <Tooltip name="Confirmed" value="{valueY.value}" />
+    );
+
+    seriesConfirmed.strokeWidth = 3;
+    seriesConfirmed.fillOpacity = 0.3;
+    seriesConfirmed.segments.template.fillModifier = fillModifier;
+
+    // Recovered
+    let seriesRecovered = x.series.push(new am4charts.LineSeries());
+    seriesRecovered.name = "Recovered";
+    seriesRecovered.dataFields.dateX = "date";
+    seriesRecovered.dataFields.valueY = "dailyrecovered";
+    seriesRecovered.stroke = "#21ba45";
+    seriesRecovered.fill = "#21ba45";
+
+    seriesRecovered.tooltipHTML = renderToStaticMarkup(
+      <Tooltip name="Recovered" value="{valueY.value}" />
+    );
+
+    seriesRecovered.strokeWidth = 3;
+    seriesRecovered.fillOpacity = 0.3;
+    seriesRecovered.segments.template.fillModifier = fillModifier;
+
+    // Deceased
+    let seriesDeceased = x.series.push(new am4charts.LineSeries());
+    seriesDeceased.name = "Deceased";
+    seriesDeceased.dataFields.dateX = "date";
+    seriesDeceased.dataFields.valueY = "dailydeceased";
+    seriesDeceased.stroke = "#767676";
+    seriesDeceased.fill = "#767676";
+    seriesDeceased.strokeWidth = 3;
+    seriesDeceased.fillOpacity = 0.3;
+
+    seriesDeceased.tooltipHTML = renderToStaticMarkup(
+      <Tooltip name="Deceased" value="{valueY.value}" />
+    );
+
+    seriesDeceased.segments.template.fillModifier = fillModifier;
+
+    x.cursor = new am4charts.XYCursor();
+
+    let scrollbarX = new am4charts.XYChartScrollbar();
+    scrollbarX.series.push(seriesConfirmed);
+    scrollbarX.series.push(seriesRecovered);
+    scrollbarX.series.push(seriesDeceased);
+    x.scrollbarX = scrollbarX;
+    // Pre Zoom
+    x.events.on("ready", function () {
+      dateAxis.zoomToDates(
+        new Date().setMonth(new Date().getMonth() - 6), // 6 months ago
+        new Date(),
+        false,
+        true // this makes zoom instant
+      );
+    });
+    // Lengends
+    x.legend = new am4charts.Legend();
+
+    casesChart.current = x;
+
+    return () => {
+      x.dispose();
+    };
+  },[]);
+
+  return (
+  
+    <div id="casesChartDiv" 
+      style={{ width: "100%", height: "500px" }}>
+    </div>			
+
+  )
 }
 
 export default LineChart;
